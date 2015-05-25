44b5d03f89953c58ca603a83149bcf71c0328e09
Aleh Zasypkin <azasypkin@mozilla.com>
2015-05-25 14:35:15 +0200

    Fixed gulpfile to copy standalone lib to examples folder only once standalone file is fully generated.

--- gulpfile.js
+++ gulpfile.js
@@ -3,33 +3,39 @@ var fs = require('fs');
 var browserify = require('browserify');
 var babelify = require('babelify');
 
-gulp.task('default', function () {
-  var browserifyOptions = {
-    debug: true,
-    standalone: 'FxOSWebSocket'
-  };
+var browserifyOptions = {
+  debug: true,
+  standalone: 'FxOSWebSocket'
+};
 
-  function onError(e) {
-    console.log("Error: " + e.message);
-  }
+function onError(e) {
+  console.log("Error: " + e.message);
+}
 
-  [
-    "fxos-websocket-server.js",
-    "fxos-websocket-server-standalone.js"
-  ].forEach(function(name) {
-    browserify(browserifyOptions)
-      .transform(babelify)
-      .require("./src/server.es6.js", { entry: true })
-      // Exclude external event dispatcher lib non-standalone file
-      .ignore(name.indexOf('standalone') < 0 ? 'event-dispatcher-js' : '')
-      .bundle()
-      .on('error', onError)
-      .pipe(fs.createWriteStream('dist/' + name));
-  });
+gulp.task('compile', function() {
+  return browserify(browserifyOptions)
+    .transform(babelify)
+    .require('./src/server.es6.js', { entry: true })
+    // Exclude external event dispatcher lib non-standalone file
+    .ignore('event-dispatcher-js')
+    .bundle()
+    .on('error', onError)
+    .pipe(fs.createWriteStream('dist/fxos-websocket-server.js'));
+});
 
+gulp.task('compile-standalone', function() {
+  return browserify(browserifyOptions)
+    .transform(babelify)
+    .require('./src/server.es6.js', { entry: true })
+    .bundle()
+    .on('error', onError)
+    .pipe(fs.createWriteStream('dist/fxos-websocket-server-standalone.js'));
+});
+
+gulp.task('default', ['compile', 'compile-standalone'], function () {
   // Copy standalone variant to examples folder
   gulp.src('./dist/fxos-websocket-server-standalone.js').pipe(
-    gulp.dest('./examples/ping-pong/js/lib/')
+      gulp.dest('./examples/ping-pong/js/lib/')
   );
 });
 
